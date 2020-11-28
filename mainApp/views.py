eb17af621ffa09f541b594be9579f34a3273199c
Mohammad mahdi <mohammadmahdi@ubuntu.ubuntu-domain>
2020-11-28 22:08:46 +0330

    create viwes ...

--- mainApp/views.py
+++ mainApp/views.py
@@ -7,3 +7,11 @@ from django.utils import timezone
 from .models import Lesson,Teacher,DatesOfWeek,Comment
 
 
+class LessonsView(generic.ListView):
+    template_name = 'mainApp/lesson.html'
+    context_object_name = 'latest_lesson_list'
+
+    def get_queryset(self):
+        return Lesson.objects.all()
+
+
